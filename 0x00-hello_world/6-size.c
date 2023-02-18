7db956bc7ebeb6fd57de7f1fcbe63c12773a9ea7
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-18 00:33:01 -0800

    size pf variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	float f;
-	long long lli;
-
-	printf("Size of a char: %zu bytes(s)\n", sizeof(c));
-	printf("Size of an int: %zu bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(li));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * start by the main() function
+ * call the function printf() function
+ * return 0 = stop the program
+ */
+
+int main(void)
+{
+	printf("testing .... betty");
+	return (0);
+}
