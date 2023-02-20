9d6804bed807993bd5b7099ce1f5e1bc7e825469
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-19 17:42:29 -0800

    commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int i, ten = 10;
+
+	for (i = 0; i < ten; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
