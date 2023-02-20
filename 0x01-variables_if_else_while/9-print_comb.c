812fd8d28b8a09e76315746276c505cc8ef9cf39
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-20 12:25:57 -0800

    comments

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,16 @@ int main(void)
 
 	for (i = 0; i < ten; i++)
 	{
-		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar('0' + i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('0' + i);
+		}
 	}
 	putchar('\n');
 	return (0);
