7e2162d5269d36aa45cdecdf0cff24a609c05613
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-24 19:21:59 -0800

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,17 @@
 #include "main.h"
-
 /**
- * *print_diagonal - Draws a diagonal lines according to parameter
- * *@n: The number of times to print diagonal lines
- * *Return: empty
- * */
-
+ * print_diagonal - function
+ * @n: param
+ */
 void print_diagonal(int n)
-
 {
-	int x, y;
+	int y, x;
 
-	if (n <= 0)
+	for (y = 0; y < n; y++)
 	{
+		for (x = 0; x < y; x++)
+			_putchar(' ');
+		_putchar('\\');
 		_putchar('\n');
-	}
-	else
-	{
-		for (x = 0; x < n; x++)
-		{
-			for (y = 0; y < x; y++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
 	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,4 +11,5 @@ void print_line(int n);
 void print_diagonal(int n);
 int _putchar(char c);
 void print_square(int size);
+void print_triangle(int size);
 #endif
