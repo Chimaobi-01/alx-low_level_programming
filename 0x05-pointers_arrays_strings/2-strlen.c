ff8bb7da5b1a04131fec6f00c849ccfdf9e9ccd8
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-27 15:32:26 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: a string pointer
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		len++;
+	}
+
+	return (len);	
+}
