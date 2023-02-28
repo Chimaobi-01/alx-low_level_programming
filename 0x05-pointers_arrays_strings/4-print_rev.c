1045bb997bafafa8bb403093016a771d2f02d2cd
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-27 22:18:21 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a atring in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	for (i = i; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
