c7ed16851269234265427c1ac96a79eb67c554eb
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-02-28 00:26:02 -0800

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,11 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-	}
-	for (i = i; *(s + i) != '\0'; i--)
-	{
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
