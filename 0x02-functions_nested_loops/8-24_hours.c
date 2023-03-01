46878034cb9fa30697a2347f2c3273bc3f2fb7e2
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-01 07:06:45 -0800

    not sure

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,24 +5,23 @@
 void jack_bauer(void)
 {
 	int i, x, y, z;
-		
+
 	for (z = 0; z < 3; z++)
 	{
-		for (y = 0; y < 4; y++)
+		for (y = 0; y < 10; y++)
 		{
 			for (x = 0; x < 6; x++)
 			{
-				for (i = 0; i < 10; i++) 
+				for (i = 0; i < 10; i++)
 				{
-					_putchar('0'+z);
-					_putchar('0'+y);
+					_putchar('0' + z);
+					_putchar('0' + y);
 					_putchar(':');
-					_putchar('0'+x);
-					_putchar('0'+i);
+					_putchar('0' + x);
+					_putchar('0' + i);
 					_putchar('\n');
 				}
 			}
 		}
 	}
 }
-
