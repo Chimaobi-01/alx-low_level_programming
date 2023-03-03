ef6fa4302ccdbcc6b783acd6e371a508bef83baa
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-03 14:53:11 -0800

    not yet

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, k;
+
+	for (i = 0; i < n--; i++)
+	{
+		k = a[i];
+		a[i] = a[n];
+		a[n] = k;
+	}
+}
