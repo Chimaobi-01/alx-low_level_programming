663f897b00a7e587f063bb3f19c01e2945ccbc8a
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-04 03:33:59 -0800

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			if (k > i)
+			{
+				putchar('0' + i);
+				putchar('0' + k);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
