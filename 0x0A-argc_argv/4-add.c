78fbb16c38d4f489bf69d5618a7b9d012714e108
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-10 17:29:09 -0800

    adding arguments like a pro

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
