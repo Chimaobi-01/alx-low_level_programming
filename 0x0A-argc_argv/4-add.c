dd35156cb7d5a95c40c2a5a262503cd110cf7593
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-13 12:35:12 -0700

    add only positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  * main - function
  * @argc: argument count
@@ -8,28 +9,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, k, sum = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]) && atoi(argv[i]) >= 0)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", sum);
+		if (atoi(argv[i]))
+			break;
+	}
+	if (!(i < argc))
+	{
+		printf("0\n");
 		return (0);
 	}
+	for (k = 1; k < argc; k++)
+	{
+		if (atoi(argv[k]))
+		{
+			sum += atoi(argv[k]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
