14f2beac026e09d201f97a7dd216ac53484713d0
Chinaka Chimaobi <chimaobi01@gmail.com>
2023-03-13 12:40:46 -0700

    add only positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		printf("0\n");
+		printf("%d\n", 0);
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	if (!(i < argc))
 	{
-		printf("0\n");
+		printf("%d\n", 0);
 		return (0);
 	}
 	for (k = 1; k < argc; k++)
